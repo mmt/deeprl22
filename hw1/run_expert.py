8f47fd1134a8ea4b101f18e1c1fd2ccafcb8095f
sMark M. Tobenkin <smarkmt@gmail.com>
2017-04-15 12:52:42 -0700

    Save to HDF5 files.

--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -9,39 +9,25 @@ Example usage:
 Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
 """
 
+import gym
+import h5py
+import load_policy
+import numpy as np
 import pickle
 import tensorflow as tf
-import numpy as np
 import tf_util
-import gym
-import load_policy
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('expert_policy_file', type=str)
-    parser.add_argument('envname', type=str)
-    parser.add_argument('--render', action='store_true')
-    parser.add_argument("--max_timesteps", type=int)
-    parser.add_argument('--num_rollouts', type=int, default=20,
-                        help='Number of expert roll outs')
-    args = parser.parse_args()
-
-    print('loading and building expert policy')
-    policy_fn = load_policy.load_policy(args.expert_policy_file)
-    print('loaded and built')
-
+def simulate(envname, max_timesteps, num_rollouts, policy_fn, render=False):
     with tf.Session():
         tf_util.initialize()
-
-        import gym
-        env = gym.make(args.envname)
-        max_steps = args.max_timesteps or env.spec.timestep_limit
+    
+        env = gym.make(envname)
+        max_steps = max_timesteps or env.spec.timestep_limit
 
         returns = []
         observations = []
         actions = []
-        for i in range(args.num_rollouts):
+        for i in range(num_rollouts):
             print('iter', i)
             obs = env.reset()
             done = False
@@ -54,19 +40,50 @@ def main():
                 obs, r, done, _ = env.step(action)
                 totalr += r
                 steps += 1
-                if args.render:
+                if render:
                     env.render()
-                if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+                if steps % 500 == 0: print("%i/%i"%(steps, max_steps))
                 if steps >= max_steps:
                     break
             returns.append(totalr)
+        return observations, actions, returns
 
-        print('returns', returns)
-        print('mean return', np.mean(returns))
-        print('std of return', np.std(returns))
 
-        expert_data = {'observations': np.array(observations),
-                       'actions': np.array(actions)}
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('expert_policy_file', type=str)
+    parser.add_argument('envname', type=str)
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument("--max_timesteps", type=int)
+    parser.add_argument('--num_rollouts', type=int, default=20,
+                        help='Number of expert roll outs')
+    parser.add_argument('--output_file', type=str, default=None,
+                        help='Filename to output to.')
+    
+    args = parser.parse_args()
+
+    print('loading and building expert policy')
+    policy_fn = load_policy.load_policy(args.expert_policy_file)
+    print('loaded and built')
+
+    observations, actions, returns = simulate(
+        args.envname, args.max_timesteps, args.num_rollouts, 
+        policy_fn, render=args.render)
+
+    print('returns', returns)
+    print('mean return', np.mean(returns))
+    print('std of return', np.std(returns))
+
+    f = h5py.File(args.output_file, 'w')
+    f.create_dataset('returns', data=returns)    
+    f.create_dataset('observations', data=observations)
+    f.create_dataset('actions', data=np.array(actions).squeeze())
+    f.close()
+    #expert_data = {'observations': np.array(observations),
+    #               'actions': np.array(actions).squeeze()}
+
+
 
 if __name__ == '__main__':
     main()
